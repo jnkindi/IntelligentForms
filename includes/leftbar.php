ef24f4016d03f2984ee88bd552d6054e48d97340
Jacques Nyilinkindi <jacknyilinkindi@gmail.com>
2019-05-11 21:17:06 +0200

    dynamic form

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -4,8 +4,9 @@
         <ul class="list-item">
             <li><a href="manage-forms.php">My Forms</a></li>
             <li><a href="manage-forms.php">My Surveys</a></li>
-            <li><a href="add-form.php">Create Form</a></li>
-            <li><a href="add-form.php">Create Survey</a></li>
+            <li><a href="add-form.php?form">Create Form</a></li>
+            <li><a href="add-form.php?survey">Create Survey</a></li>
+            <li><a href="add-form.php?cart">Create Cart</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="notifications.php">Notifications <span class="notinumber">5</span></a></li>
             <li><a href="index.php">Sign Out</a></li>
